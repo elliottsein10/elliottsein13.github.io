3929dd05a5cf46d2059795d2f8d97bbb8ea04588
Elliott Sánchez León <raptordroid15@gmail.com>
2022-06-15 23:07:31 -0500

    Trabajando con texto - cadenas de caracteres

---
+++ Trabajandotexto.py
@@ -0,0 +1,13 @@
+nombre = input("¿Cuál es tu nombre?: ")
+nombre.upper()
+nombre.capitalize()
+nombre = nombre.capitalize()
+nombre = nombre.strip()
+nombre = nombre.lower()
+nombre = nombre.replace('o', 'a')
+nombre[0]
+nombre[1]
+letra = nombre[2]
+len(nombre)
+len(letra)
+len("Hola! Este es el curso de python")
