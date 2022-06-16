6373d09354e6cf9b5e550ee4cdd86038d4ecee23
Elliott Sánchez León <raptordroid15@gmail.com>
2022-06-15 22:49:53 -0500

    Modularizando nuestro conversor de monedas

--- conversion.py
+++ conversion.py
@@ -1,3 +1,10 @@
+def conversor(tipo_pesos, valor_dolar):
+    pesos = input("¿Cuántos pesos" + tipo_pesos + "tienes?: ")
+    pesos = float(pesos)
+    dolares = pesos / valor_dolar
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dolares")
+
 menu = """
 Bienvenido al conversor de monedas
 
@@ -10,26 +17,11 @@ Elige una opción """
 opcion = input(menu)
 
 if opcion == 1:
-    pesos = input("¿Cuántos pesos colombianos tienes?: ")
-    pesos = float(pesos)
-    valor_dolar = 3875
-    dolares = pesos / valor_dolar
-    dolares = str(dolares)
-    print("Tienes $" + dolares + " dolares")
+    conversor("colombianos", 3875)
 elif opcion == 2:
-    pesos = input("¿Cuántos pesos argentinos tienes?: ")
-    pesos = float(pesos)
-    valor_dolar = 65
-    dolares = pesos / valor_dolar
-    dolares = str(dolares)
-    print("Tienes $" + dolares + " dolares")
+    conversor("argentinos", 65)
 elif opcion == 3:
-    pesos = input("¿Cuántos pesos mexicanos tienes?: ")
-    pesos = float(pesos)
-    valor_dolar = 24
-    dolares = pesos / valor_dolar
-    dolares = str(dolares)
-    print("Tienes $" + dolares + " dolares")
+    conversor("mexicanos", 24)
 else:
     print('Ingresa una opción correcta por favor')
 
